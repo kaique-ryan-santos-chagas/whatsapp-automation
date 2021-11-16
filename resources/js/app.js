b074688079729bc183543d40bd8748a2988e23f0
= <=>
2021-11-16 15:40:34 -0300

    Trabalhando na ferramenta de automação de mensagens no whatsapp

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+require('./bootstrap');
+
+
+console.log('Hello World');
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <title>Automação do Whatsapp</title>
+
+    </head>
+
+    <body style="background-color: #F8F8FF">
+
+        <div class="w3-card-5">
+            
+            <div class="w3-container w3-card" id="title-container" style="background-color: #25D366">
+                <img 
+                    src="https://www.aps-personal.com/wp-content/uploads/2016/12/Whatsapp_Icon.png"
+                    class="w3-left w3-animate-bottom"
+                    style="width: 30px; margin-top: 15px"
+                />
+
+               <h3 class="w3-left w3-animate-bottom w3-text-white" style="margin-left: 15px">
+                    <b>Automação do Whatsapp</b>
+               </h3>
+
+            </div>
+
+        </div>
+
+
+        <div class="w3-right">
+            <img 
+                src="https://th.bing.com/th/id/R.2ffbac4c15dc242e7edc77e91a76f545?rik=wQlF6MjRdw2%2bPA&riu=http%3a%2f%2fwww.fl-esi.com%2fwp-content%2fuploads%2f2019%2f03%2fAdobeStock_163871333.jpeg&ehk=5NWfSmsxRWHyWok62usl02VnhjkTJ2PqVtz3SYvlp0U%3d&risl=&pid=ImgRaw&r=0" 
+                class="w3-round w3-animate-bottom w3-card"
+                style="margin-top: 80px; margin-right: 80px; width: 520px; height: 400px"
+            />
+        </div>
+
+
+        <div class="w3-bottom">
+            <img 
+                src="https://is1-ssl.mzstatic.com/image/thumb/Purple118/v4/b5/e2/56/b5e256d5-97ef-a9e4-eb73-bc85d904b021/source/512x512bb.jpg"  
+                style="width: 60px; margin-left: 10px; margin-bottom: 5px"
+            />
+        </div>
+
+        <div class="w3-text-gray" style="bottom: 0; right: 0; position: absolute; margin-right: 60px; margin-bottom: 10px">
+            <p>Global Hitss - 2021</p>
+        </div>
+           
+    </body>
+
+</html>
+
+
